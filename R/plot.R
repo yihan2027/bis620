9be1ae6cae65765a9f322ed7d5a5810b78de362c
刘以涵 <Alice@MacBook-Air-2.local>
2022-11-16 01:22:37 -0500

    Increase the test coverage, call lint()to fix issues, add a action to test coverage.

--- R/plot.R
+++ R/plot.R
@@ -6,22 +6,22 @@
 #' either a `time` or `freq` column.
 #' @return a time-series plot faceted by axis.
 #' @importFrom tidyr pivot_longer all_of
-#' @importFrom ggplot2 aes_string ggplot geom_line facet_grid
+#' @importFrom ggplot2 aes geom_line facet_grid
 #' @examples
 #' data(ukb_accel)
 #' accel_plot(ukb_accel[1:1000,])
 #' @export
-accel_plot = function(x) {
+accel_plot <- function(x) {
   if ("time" %in% colnames(x)) {
-    col = "time"
+    col <- "time"
   } else if ("freq" %in% colnames(x)) {
-    col = "freq"
+    col <- "freq"
   } else {
     stop("A `time` or a `freq` column must appear in the data.")
   }
   x |>
     pivot_longer(-all_of(col)) |>
-    ggplot(aes_string(x = col, y = "value")) +
+    ggplot(ggplot2::aes(x = {{col}}, y = value)) +
       geom_line() +
       facet_grid(name ~ .)
 }
--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -2,7 +2,7 @@ test_that(
   "The accel_plot() returns a ggplot object.",
   {
     data(ukb_accel)
-    p =  accel_plot(ukb_accel[1:100,])
+    p <-  accel_plot(ukb_accel[1:100, ])
     expect_true(inherits(p, "gg"))
   }
 )
@@ -19,7 +19,17 @@ test_that(
   "The accel_plot() is correct for time-series data.",
   {
     data(ukb_accel)
-    p = accel_plot(ukb_accel[1:100,])
-    expect_doppelganger("first-100-samples", p) #does the plot look the same as what we see in the snapshot
+    p <- accel_plot(ukb_accel[1:100, ])
+    vdiffr::expect_doppelganger("first-100-samples", p)
+    #does the plot look the same as what we see in the snapshot
+  }
+)
+
+test_that(
+  "The accel_plot() is correct for freq data.",
+  {
+    data("ukb_accel")
+    p <- accel_plot(ukb_accel[1:100, ] |> spectral_signature(take_log = TRUE))
+    vdiffr::expect_doppelganger("first-100-samples-freq", p)
   }
 )
