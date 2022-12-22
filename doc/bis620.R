9bb6a9b191f6364118774d275ccf8c9701397113
刘以涵 <Alice@MacBook-Air-2.local>
2022-12-21 19:37:30 -0500

    Updated Vignettes.

--- doc/bis620.R
+++ doc/bis620.R
@@ -7,3 +7,73 @@ knitr::opts_chunk$set(
 ## ----setup--------------------------------------------------------------------
 library(bis620.2022)
 
+## ---- include = FALSE---------------------------------------------------------
+library(knitr)
+library(dplyr)
+library(kableExtra)
+library(gtsummary)
+
+## -----------------------------------------------------------------------------
+#load in data
+data(dl)
+dl$adsl |>
+  select(ATRT, DTHDY, DTH, PFSDYCR, PFSCR, AGE, SEX) |>
+  tbl_summary(by = ATRT)
+
+## -----------------------------------------------------------------------------
+kras(dl$biomark, mutant_standard = 1) |> group_by(arm) |> summarize(n = n()) |>
+  kable()
+
+## -----------------------------------------------------------------------------
+kras(dl$biomark, mutant_standard = 2) |> group_by(arm) |> summarize(n = n()) |>
+  kable()
+
+## ---- fig.align='center', fig.dim = c(6, 3.5)---------------------------------
+survival_plot(kras(dl$biomark, 1), "PFS", "Treatment", "Wild-type")
+log_rank(kras(dl$biomark, 1), "PFS", "Treatment", "Wild-type")$median
+
+## -----------------------------------------------------------------------------
+log_rank(kras(dl$biomark, 1), "PFS", "Treatment", "Wild-type")$p
+
+## ---- fig.align='center', fig.dim = c(6, 3.5)---------------------------------
+survival_plot(kras(dl$biomark, 2), "PFS", "Treatment", "Wild-type")
+log_rank(kras(dl$biomark, 2), "PFS", "Treatment", "Wild-type")$median
+
+## -----------------------------------------------------------------------------
+log_rank(kras(dl$biomark, 2), "PFS", "Treatment", "Wild-type")$p
+
+## ---- fig.align='center', fig.dim = c(6, 3.5)---------------------------------
+survival_plot(kras(dl$biomark, 1), "OS", "Treatment", "Wild-type")
+log_rank(kras(dl$biomark, 1), "OS", "Treatment", "Wild-type")$median
+
+## -----------------------------------------------------------------------------
+log_rank(kras(dl$biomark, 1), "OS", "Treatment", "Wild-type")$p
+
+## ---- fig.align='center', fig.dim = c(6, 3.5)---------------------------------
+survival_plot(kras(dl$biomark, 1), "PFS", "Treatment", "Mutant")
+log_rank(kras(dl$biomark, 1), "PFS", "Treatment", "Mutant")$median
+
+## -----------------------------------------------------------------------------
+log_rank(kras(dl$biomark, 1), "PFS", "Treatment", "Mutant")$p
+
+## -----------------------------------------------------------------------------
+adverse_event(dl$adae, "Wild-type") |> kable() |> kable_styling(font_size = 12)
+
+## -----------------------------------------------------------------------------
+ae_surv(dl$adae, "Rash") |>
+  select(SUBJID, ATRT, BMK, DTHDY, DTH, PFSDYCR, PFSCR, RASH_GRADE) |>
+  head(5) |>
+  kable()
+
+## ---- fig.align='center', fig.dim = c(6, 3.5)---------------------------------
+survival_plot(ae_surv(dl$adae, "Rash") |>
+                filter(ATRT == "Panitumumab + FOLFOX"),
+              "PFS", "Rash Grade", "Wild-type")
+log_rank(ae_surv(dl$adae, "Rash") |>
+           filter(ATRT == "Panitumumab + FOLFOX"),
+         "PFS", "Rash Grade", "Wild-type")$median
+
+## -----------------------------------------------------------------------------
+log_rank(ae_surv(dl$adae, "Rash")|> filter(ATRT == "Panitumumab + FOLFOX"),
+         "PFS", "Rash Grade", "Wild-type")$p
+
